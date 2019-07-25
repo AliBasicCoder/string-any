bb05e0496e546868949bdf6906498cc117664cc0
AliBasicCoder <aliahmedreda34@gmail.com>
2019-07-25 22:43:25 +0200

    ..

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-/**
- * @returns {string}
- * @param {string} key
- */
+
 function fixKey(key) {
   if (/[^\w]/.test(key) || /\d/.test(key)) {
     return `"${key}"`;
@@ -9,23 +6,22 @@ function fixKey(key) {
   return key;
 }
 
-/**
- * @returns {string}
- * @param {string} str 
- */
 function addTab(str) {
   let res = str.replace(/\r\n/g, '\n');
   res = res.replace(/\r/g, '\n');
-  const res2 = res.split('\n');
-  for (let i = 0; i < res2.length; i++) {
-    if (!(i === (res2.length - 1) || i === 0)) {
-      res2[i] = '   ' + res2[i];
+  const arr = res.split('\n');
+
+  const callback = (item, i) => {
+    if (!(i === (arr.length - 1) || i === 0)) {
+      item = '   ' + item;
     }
-    if (i === (res2.length - 1)) {
-      res2[i] = `  ${res2[i]}`;
+    if (i === (arr.length - 1)) {
+      item = `  ${item}`;
     }
+    return item;
   }
-  return res2.join('\n');
+
+  return arr.map(callback).join('\n');
 }
 
 function toStr(thing) {
@@ -37,19 +33,18 @@ function toStr(thing) {
   if (Array.isArray(thing)) {
     let toReturn = '[';
 
-    for (let i = 0; i < thing.length; i++) {
-      toReturn += `${toStr(thing[i])}${i === (thing.length - 1) ? '' : ','}`;
-    }
+    thing.forEach(
+      (value, i) => {
+        const comma = i === (thing.length - 1) ? '' : ',';
+        toReturn += `${toStr(value)}${comma}`;
+      }
+    );
 
     toReturn += ']';
     return toReturn;
   }
 
-  if (typeof thing === 'number') {
-    return thing.toString();
-  }
-
-  if (typeof thing === 'function') {
+  if (typeof thing === 'number' || typeof thing === 'function') {
     return thing.toString();
   }
 
@@ -57,16 +52,20 @@ function toStr(thing) {
     let toReturn = '{';
     const keys = Object.keys(thing);
 
-    for (let i = 0; i < keys.length; i++) {
-      const key = keys[i];
-      let val = toStr(thing[key]);
+    keys.forEach(
+      (key, i) => {
+        const elm = thing[key];
+        let val = toStr(elm);
 
-      if (typeof thing[key] === 'object') {
-        val = addTab(toStr(thing[key]));
+        if (typeof elm === 'object') {
+          val = addTab(toStr(elm));
+        }
+
+        const comma = i === (keys.length - 1) ? '\n' : ',';
+
+        toReturn += `\n  ${fixKey(key)}: ${val}${comma}`;
       }
-
-      toReturn += `\n  ${fixKey(key)}: ${val}${i === (keys.length - 1) ? '\n' : ','}`;
-    }
+    );
 
     toReturn += '}';
     return toReturn;
